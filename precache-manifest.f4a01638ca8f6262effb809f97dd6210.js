38a51f7c51fcf138383926bbd78d60ae42b1452f
SuRoryZ <ri4kasmail@gmail.com>
2020-09-13 08:51:25 -0400

    Updates

---
+++ precache-manifest.f4a01638ca8f6262effb809f97dd6210.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7fbd212eb577246c9727",
+    "url": "/osu-vk-app/static/js/main.b2ebbb07.chunk.js"
+  },
+  {
+    "revision": "d3be112e29d15a88553c",
+    "url": "/osu-vk-app/static/js/runtime~main.24103fb1.js"
+  },
+  {
+    "revision": "65eaf638e8fb3ffcdaac",
+    "url": "/osu-vk-app/static/css/2.76ec784f.chunk.css"
+  },
+  {
+    "revision": "65eaf638e8fb3ffcdaac",
+    "url": "/osu-vk-app/static/js/2.fe5b5d0c.chunk.js"
+  },
+  {
+    "revision": "fa6624b745d6d59345b56bf6fca99f8f",
+    "url": "/osu-vk-app/index.html"
+  }
+];
